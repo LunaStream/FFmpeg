e3df5fbb26c4dd3de0a475c8f3f3f71d560c2ff9
Alice w/🌧️ <rainyxeon@gmail.com>
2025-05-01 18:49:35 +0700

    remove: weird option

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ cat <<EOF >"$BUILD_SCRIPT"
       --enable-decoder=mp3,flac,vorbis,opus,aac,pcm_s16le \
       --enable-encoder=mp3,flac,vorbis,opus,aac,pcm_s16le \
       --enable-parser=mp3,flac,aac,vorbis \
-      --disable-libmfx --disable-qsv \
+      --disable-libmfx --disable-vaapi \
       --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS \$FF_CONFIGURE \
       --extra-cflags="\$FF_CFLAGS" --extra-cxxflags="\$FF_CXXFLAGS" --extra-libs="\$FF_LIBS" \
       --extra-ldflags="\$FF_LDFLAGS" --extra-ldexeflags="\$FF_LDEXEFLAGS" \
